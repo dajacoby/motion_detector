13688315056402d20ecb2121af6b2d17f3dc31e1
Daniel <Daniel.Jacoby@me.com>
2019-09-29 19:47:34 +0200

    my first Arduino Project

---
+++ mdetector/mdetector.ino
@@ -0,0 +1,49 @@
+int relayPin = 3;     // choose the output pin for relay
+int val = 0;          // start with relayPIN is LOW
+int inputPin = 2;     // choose the input pin for PIR sensor
+int pirState = LOW;   // start with no motion detected
+int LDRinput = A3;    // choose the input pin for LDR sensor
+int sensorwert=0;     // start with 0 for LDR sensor  
+int input = A0;       // choose the input pin for Rotary Potentiometer
+int reglerwert=0;     // start with 0 for Rotary Potentiometer  
+int button = 6;       // choose the input pin for button (light on for xx time) 
+int buttonstatus=0;   // start with no button pushed
+int yellowLED = 4;    // choose the output pin for yellow LED (motion detection on)
+
+void setup() 
+{ 
+  pinMode(relayPin, OUTPUT);
+  pinMode(yellowLED, OUTPUT);
+  pinMode(inputPin, INPUT);
+  pinMode(button, INPUT);
+} 
+ 
+void loop(){
+  buttonstatus=digitalRead(button);
+  if (buttonstatus == HIGH){          // if button pushed then
+    digitalWrite(relayPin,HIGH);      // relayPIN (light on)
+    delay(5000);                      // wait for a second
+    digitalWrite(relayPin,LOW);       // relayPIN (light off)
+ }
+  else {  
+    sensorwert=analogRead(LDRinput);
+    reglerwert=analogRead(input);
+  if (sensorwert < reglerwert) {
+    digitalWrite(yellowLED,HIGH);     // yellow LED on
+    val = digitalRead(inputPin);      // read relayPIN 
+  if (val == HIGH) {
+    do{                               // start with do-while
+    digitalWrite(relayPin, HIGH);     // turn the relay on (HIGH is the voltage level)
+    delay(10000);                      // wait for a second
+    }while(digitalRead(inputPin));    // read the PIR sensor for replay the do-while
+  //  delay(5000);
+  } else {
+      digitalWrite(relayPin, LOW);      // turn the relay off by making the voltage LOW
+  }
+  } else {
+      digitalWrite(yellowLED, LOW);
+      digitalWrite(relayPin, LOW);
+  }
+  }
+}
+ 
